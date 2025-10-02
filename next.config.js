b42bbf7268e4a9331893c6e950e41e7ba619a9c2
miekxd <mike.nguyen0105@gmail.com>
2025-10-02 11:58:07 +1000

    f: nixpack deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // App Router is enabled by default in Next.js 13+
+  output: 'standalone',
+  experimental: {
+    outputFileTracingRoot: process.cwd(),
+  },
 }
 
 module.exports = nextConfig
